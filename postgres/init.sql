f0438bcbeb9ec0a69f5df4c1aef1ed3668b6e83f
Or Ben Chitrit <10655932+Orbsynated@users.noreply.github.com>
2025-01-26 18:07:19 +0200

    wip

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,8 +1,8 @@
 -- Create tenant table (just for the example)
 CREATE TABLE tenants
 (
-    tenant_id   UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    tenant_name VARCHAR(255) UNIQUE NOT NULL
+    id   UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
 
 
